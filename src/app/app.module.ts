6bb7f20e6ce22a7a9dbce44d4a05eda882c19422
unknown <cyntthiah@gmail.com>
2018-09-18 23:40:38 -0500

    cambios en login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,42 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './user/user.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatMenu, MatMenuModule, MatIconModule, MatListModule} from '@angular/material';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    AdminComponent,
+    HomeComponent,
+    UserComponent
+  
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatIconModule,
+    MatListModule,
+    NgbModule
+
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class PizzaPartyAppModule { }
+export class NgbdButtonsCheckbox {
+  model = {
+    left: true,
+    middle: false,
+    right: false
+  };
+}
