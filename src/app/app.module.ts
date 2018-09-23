729ec76d8803ba5714c12adee0075fcdc0c3aeb8
unknown <cyntthiah@gmail.com>
2018-09-22 21:08:46 -0500

    todo ha sido modificado, loguin y registro con firebase son funcionales

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,59 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-//firebase
-import { AngularFireModule } from '@angular/fire';
-import { environment } from '../environments/environment';
-import { AngularFireAuthModule } from '@angular/fire/auth';
-
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { AdminComponent } from './admin/admin.component';
-import { HomeComponent } from './home/home.component';
-import { UserComponent } from './user/user.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatButtonModule, MatCheckboxModule, MatMenu, MatMenuModule, MatIconModule, MatListModule} from '@angular/material';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import { WallComponent } from './wall/wall.component';
-import {RouterModule, Routes} from '@angular/router';
 
-const appRoutes: Routes = [
-  { path: '', component: LoginComponent }
- 
-];
+//Firebase
+import {AngularFireModule} from '@angular/fire';
+import {AngularFireAuthModule} from '@angular/fire/auth';
+
+//form
+import { ReactiveFormsModule } from '@angular/forms';
+
+//auth
+import {AuthService} from './auth.service';
+import { environment } from '../environments/environment';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    AdminComponent,
-    HomeComponent,
-    UserComponent,
-    WallComponent
-  
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
-    MatMenuModule,
-    MatIconModule,
-    MatListModule,
-    NgbModule,
+    AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
-  RouterModule.forRoot(appRoutes),
-  AngularFireModule.initializeApp(environment.firebase),
-    ],
-  
-  // providers: [AuthService],
+    ReactiveFormsModule
+  ],
+  providers: [AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
-export class PizzaPartyAppModule { }
-export class NgbdButtonsCheckbox {
-  model = {
-    left: true,
-    middle: false,
-    right: false
-  };
-}
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {AuthService} from '../auth.service';
+
+
 
 @Component({
   selector: 'app-login',
@@ -6,10 +10,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  authForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {
+    this.createAuthForm();
+  }
 
   ngOnInit() {
   }
+  createAuthForm() {
+    this.authForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      password: ['', Validators.compose([Validators.required, Validators.minLength(4)])]
+    });
+  }
+  onRegister() {
+    this.authService.signup(this.authForm.value.email, this.authForm.value.password)
+      .then(() => {
+        //Registro exitoso, celebremos esto!
+        alert("Registro con exito");
+      })
+      .catch(() => {
+        //Algo salió mal, avisemos mejor para que reintente
+        alert("Algo salio mal");
+      });
+  }
+  onLogin() {
+    this.authService.login(this.authForm.value.email, this.authForm.value.password)
+      .then(() => {
+        //Login exitoso, así que celebramos con el usuario (?)
+        alert("Login Con exito");
+      })
+      .catch(() => {
+        //Algo salió mal, avisemos mejor para que reintente
+        alert("Algo Salio mal");
+      });
+  }
 
+  onLogout() {
+    this.authService.logout()
+      .then(() => {
+        //Logout exitoso, adios usuario!
+        alert("Te juiste");
+      })
+      .catch(() => {
+        //Algo salió mal, avisemos mejor para que reintente
+        alert("Algo salio mal");
+      });
+  }
 }
+
