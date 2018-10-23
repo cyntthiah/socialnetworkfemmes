88e8f3a9c3633c232f31f560f635362ed1bfabbc
cyntthiah <cyntthiah@gmail.com>
2018-10-23 10:29:31 -0300

    listo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   firebase : {
-      apiKey: 'AIzaSyBpK01dnnVX_TEzS3F127sfyfcAJRRTZ28',
+      apiKey: '"AIzaSyBBnHAY9vKgM269fKNPFOrtOH30gNDnW5E"',
       authDomain: 'socialnetwork2-36f58.firebaseapp.com',
       databaseURL: 'https://socialnetwork2-36f58.firebaseio.com',
       projectId: 'socialnetwork2-36f58',
